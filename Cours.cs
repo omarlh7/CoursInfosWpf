46b2b6cae87125f23a9e8b1508031507ed49f4ae
omlah6 <omar.lahyani.1@ulaval.ca>
2024-10-12 18:56:36 -0400

    Add project files.

---
+++ Cours.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursInfosWpf
+{
+    public class Cours
+    {
+        public string Nom { get; set; }
+        public string Code { get; set; }
+        public int NombreHeure { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoursInfosWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Cours> ListeCours { get; set; }
+        public Cours SelectedCourse { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Liaison du DataContext à la fenêtre pour permettre le binding
+            this.DataContext = this;
+
+            // Initialisation de la liste avec des cours
+            ListeCours = new ObservableCollection<Cours>
+{
+new Cours { Nom = "Gestion", Code = "1", NombreHeure = 40 },
+new Cours { Nom = "Programmation", Code = "2", NombreHeure = 35 },
+new Cours { Nom = "Developpement WEB", Code = "3", NombreHeure = 40 }
+};
+        }
+
+    }
+}
